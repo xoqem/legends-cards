e0cf94eb10166e50c9340de26bf8eedaf526b653
xoqem <xoqem@gmail.com>
2020-05-06 00:15:15 -0700

    Updates

---
+++ precache-manifest.f3ff2b9865be8b70587080d14fea5b91.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f18cef2d316462e11f848fd479807288",
+    "url": "/legends-cards/index.html"
+  },
+  {
+    "revision": "279f95c2f89611ee03fa",
+    "url": "/legends-cards/static/css/main.ebfedf89.chunk.css"
+  },
+  {
+    "revision": "103e1c2ab0ce412a838c",
+    "url": "/legends-cards/static/js/2.ad4ba915.chunk.js"
+  },
+  {
+    "revision": "90565ba0b43e1b95d2876295dfd7014d",
+    "url": "/legends-cards/static/js/2.ad4ba915.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "279f95c2f89611ee03fa",
+    "url": "/legends-cards/static/js/main.bb6d486b.chunk.js"
+  },
+  {
+    "revision": "91c2a86936ab24f40761",
+    "url": "/legends-cards/static/js/runtime-main.8fc5b037.js"
+  }
+]);
